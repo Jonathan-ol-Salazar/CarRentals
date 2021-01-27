870f3bb9c9b227aa4b90c8d2db25310d5a8ebc6c
salazar <WussubiNinja@live.com>
2021-01-27 17:33:28 +1000

    Added GroupBox and Buttons to Fleet View

--- CarRentals/MainView.Designer.cs
+++ CarRentals/MainView.Designer.cs
@@ -33,20 +33,25 @@ namespace CarRentals
             this.FleetTab = new System.Windows.Forms.TabPage();
             this.DataGridView_Fleet = new System.Windows.Forms.DataGridView();
             this.CustomersTab = new System.Windows.Forms.TabPage();
-            this.RentalReportTab = new System.Windows.Forms.TabPage();
-            this.RentalSearchTab = new System.Windows.Forms.TabPage();
             this.DataGridView_Customers = new System.Windows.Forms.DataGridView();
+            this.RentalReportTab = new System.Windows.Forms.TabPage();
             this.DataGridView_RentalReport = new System.Windows.Forms.DataGridView();
+            this.RentalSearchTab = new System.Windows.Forms.TabPage();
             this.DataGridView_RentalSearch = new System.Windows.Forms.DataGridView();
+            this.GroupBox_Fleet = new System.Windows.Forms.GroupBox();
+            this.GroupBox_Fleet_Button_Modify = new System.Windows.Forms.Button();
+            this.GroupBox_Fleet_Button_Remove = new System.Windows.Forms.Button();
+            this.GroupBox_Fleet_Button_Add = new System.Windows.Forms.Button();
             this.TabControl_MainView.SuspendLayout();
             this.FleetTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Fleet)).BeginInit();
             this.CustomersTab.SuspendLayout();
-            this.RentalReportTab.SuspendLayout();
-            this.RentalSearchTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Customers)).BeginInit();
+            this.RentalReportTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_RentalReport)).BeginInit();
+            this.RentalSearchTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_RentalSearch)).BeginInit();
+            this.GroupBox_Fleet.SuspendLayout();
             this.SuspendLayout();
             // 
             // TabControl_MainView
@@ -64,6 +69,7 @@ namespace CarRentals
             // 
             // FleetTab
             // 
+            this.FleetTab.Controls.Add(this.GroupBox_Fleet);
             this.FleetTab.Controls.Add(this.DataGridView_Fleet);
             this.FleetTab.Location = new System.Drawing.Point(4, 24);
             this.FleetTab.Name = "FleetTab";
@@ -76,11 +82,10 @@ namespace CarRentals
             // DataGridView_Fleet
             // 
             this.DataGridView_Fleet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridView_Fleet.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DataGridView_Fleet.Location = new System.Drawing.Point(3, 3);
             this.DataGridView_Fleet.Name = "DataGridView_Fleet";
             this.DataGridView_Fleet.RowTemplate.Height = 25;
-            this.DataGridView_Fleet.Size = new System.Drawing.Size(786, 416);
+            this.DataGridView_Fleet.Size = new System.Drawing.Size(786, 350);
             this.DataGridView_Fleet.TabIndex = 1;
             this.DataGridView_Fleet.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -95,6 +100,16 @@ namespace CarRentals
             this.CustomersTab.Text = "Customers";
             this.CustomersTab.UseVisualStyleBackColor = true;
             // 
+            // DataGridView_Customers
+            // 
+            this.DataGridView_Customers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_Customers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView_Customers.Location = new System.Drawing.Point(3, 3);
+            this.DataGridView_Customers.Name = "DataGridView_Customers";
+            this.DataGridView_Customers.RowTemplate.Height = 25;
+            this.DataGridView_Customers.Size = new System.Drawing.Size(786, 416);
+            this.DataGridView_Customers.TabIndex = 0;
+            // 
             // RentalReportTab
             // 
             this.RentalReportTab.Controls.Add(this.DataGridView_RentalReport);
@@ -105,6 +120,16 @@ namespace CarRentals
             this.RentalReportTab.TabIndex = 2;
             this.RentalReportTab.Text = "Rental Report";
             this.RentalReportTab.UseVisualStyleBackColor = true;
+            // 
+            // DataGridView_RentalReport
+            // 
+            this.DataGridView_RentalReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_RentalReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView_RentalReport.Location = new System.Drawing.Point(3, 3);
+            this.DataGridView_RentalReport.Name = "DataGridView_RentalReport";
+            this.DataGridView_RentalReport.RowTemplate.Height = 25;
+            this.DataGridView_RentalReport.Size = new System.Drawing.Size(786, 416);
+            this.DataGridView_RentalReport.TabIndex = 1;
             // 
             // RentalSearchTab
             // 
@@ -117,26 +142,6 @@ namespace CarRentals
             this.RentalSearchTab.Text = "Rental Search";
             this.RentalSearchTab.UseVisualStyleBackColor = true;
             // 
-            // DataGridView_Customers
-            // 
-            this.DataGridView_Customers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridView_Customers.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DataGridView_Customers.Location = new System.Drawing.Point(3, 3);
-            this.DataGridView_Customers.Name = "DataGridView_Customers";
-            this.DataGridView_Customers.RowTemplate.Height = 25;
-            this.DataGridView_Customers.Size = new System.Drawing.Size(786, 416);
-            this.DataGridView_Customers.TabIndex = 0;
-            // 
-            // DataGridView_RentalReport
-            // 
-            this.DataGridView_RentalReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridView_RentalReport.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DataGridView_RentalReport.Location = new System.Drawing.Point(3, 3);
-            this.DataGridView_RentalReport.Name = "DataGridView_RentalReport";
-            this.DataGridView_RentalReport.RowTemplate.Height = 25;
-            this.DataGridView_RentalReport.Size = new System.Drawing.Size(786, 416);
-            this.DataGridView_RentalReport.TabIndex = 1;
-            // 
             // DataGridView_RentalSearch
             // 
             this.DataGridView_RentalSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -146,6 +151,45 @@ namespace CarRentals
             this.DataGridView_RentalSearch.RowTemplate.Height = 25;
             this.DataGridView_RentalSearch.Size = new System.Drawing.Size(786, 416);
             this.DataGridView_RentalSearch.TabIndex = 1;
+            // 
+            // GroupBox_Fleet
+            // 
+            this.GroupBox_Fleet.Controls.Add(this.GroupBox_Fleet_Button_Add);
+            this.GroupBox_Fleet.Controls.Add(this.GroupBox_Fleet_Button_Remove);
+            this.GroupBox_Fleet.Controls.Add(this.GroupBox_Fleet_Button_Modify);
+            this.GroupBox_Fleet.Location = new System.Drawing.Point(8, 359);
+            this.GroupBox_Fleet.Name = "GroupBox_Fleet";
+            this.GroupBox_Fleet.Size = new System.Drawing.Size(776, 55);
+            this.GroupBox_Fleet.TabIndex = 2;
+            this.GroupBox_Fleet.TabStop = false;
+            this.GroupBox_Fleet.Text = "Modify Fleet";
+            // 
+            // GroupBox_Fleet_Button_Modify
+            // 
+            this.GroupBox_Fleet_Button_Modify.Location = new System.Drawing.Point(6, 22);
+            this.GroupBox_Fleet_Button_Modify.Name = "GroupBox_Fleet_Button_Modify";
+            this.GroupBox_Fleet_Button_Modify.Size = new System.Drawing.Size(75, 23);
+            this.GroupBox_Fleet_Button_Modify.TabIndex = 0;
+            this.GroupBox_Fleet_Button_Modify.Text = "Modify";
+            this.GroupBox_Fleet_Button_Modify.UseVisualStyleBackColor = true;
+            // 
+            // GroupBox_Fleet_Button_Remove
+            // 
+            this.GroupBox_Fleet_Button_Remove.Location = new System.Drawing.Point(87, 22);
+            this.GroupBox_Fleet_Button_Remove.Name = "GroupBox_Fleet_Button_Remove";
+            this.GroupBox_Fleet_Button_Remove.Size = new System.Drawing.Size(75, 23);
+            this.GroupBox_Fleet_Button_Remove.TabIndex = 1;
+            this.GroupBox_Fleet_Button_Remove.Text = "Remove";
+            this.GroupBox_Fleet_Button_Remove.UseVisualStyleBackColor = true;
+            // 
+            // GroupBox_Fleet_Button_Add
+            // 
+            this.GroupBox_Fleet_Button_Add.Location = new System.Drawing.Point(695, 22);
+            this.GroupBox_Fleet_Button_Add.Name = "GroupBox_Fleet_Button_Add";
+            this.GroupBox_Fleet_Button_Add.Size = new System.Drawing.Size(75, 23);
+            this.GroupBox_Fleet_Button_Add.TabIndex = 2;
+            this.GroupBox_Fleet_Button_Add.Text = "Add";
+            this.GroupBox_Fleet_Button_Add.UseVisualStyleBackColor = true;
             // 
             // MainView
             // 
@@ -160,11 +204,12 @@ namespace CarRentals
             this.FleetTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Fleet)).EndInit();
             this.CustomersTab.ResumeLayout(false);
-            this.RentalReportTab.ResumeLayout(false);
-            this.RentalSearchTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Customers)).EndInit();
+            this.RentalReportTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_RentalReport)).EndInit();
+            this.RentalSearchTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView_RentalSearch)).EndInit();
+            this.GroupBox_Fleet.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -180,6 +225,10 @@ namespace CarRentals
         private System.Windows.Forms.DataGridView DataGridView_Customers;
         private System.Windows.Forms.DataGridView DataGridView_RentalReport;
         private System.Windows.Forms.DataGridView DataGridView_RentalSearch;
+        private System.Windows.Forms.GroupBox GroupBox_Fleet;
+        private System.Windows.Forms.Button GroupBox_Fleet_Button_Add;
+        private System.Windows.Forms.Button GroupBox_Fleet_Button_Remove;
+        private System.Windows.Forms.Button GroupBox_Fleet_Button_Modify;
     }
 }
 
